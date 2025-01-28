8546ece328f9a86468332d6863b7ae51d08718d4
MateoArias-fit <jmariasfeu@gmail.com>
2025-01-29 04:12:56 +0800

    Update and rename aa.JS to aa.js

---
+++ aa.js
@@ -0,0 +1,10 @@
+document.getElementById("colorChangeBtn").addEventListener("click", function () {
+    const resumeContainer = document.querySelector(".resume-container");
+    const currentColor = getComputedStyle(resumeContainer).backgroundColor;
+
+    if (currentColor === "rgb(255, 255, 255)") {
+        resumeContainer.style.backgroundColor = "#f4f4f4";
+    } else {
+        resumeContainer.style.backgroundColor = "#ffffff"; 
+    }
+});
