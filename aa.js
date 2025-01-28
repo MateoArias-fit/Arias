7e5ef52d2233ed850ef11968bf249e76b6abbe4d
MateoArias-fit <jmariasfeu@gmail.com>
2025-01-29 04:19:18 +0800

    Update aa.js

--- aa.js
+++ aa.js
@@ -2,8 +2,8 @@ document.getElementById("colorChangeBtn").addEventListener("click", function () 
     const resumeContainer = document.querySelector(".resume-container");
     const currentColor = getComputedStyle(resumeContainer).backgroundColor;
 
-    if (currentColor === "rgb(255, 255, 255)") {
-        resumeContainer.style.backgroundColor = "#88a6c6";
+    if (currentColor === "rgb(255, 255, 255)") { 
+        resumeContainer.style.backgroundColor = "#ADD8E6";
     } else {
         resumeContainer.style.backgroundColor = "#ffffff";
     }
